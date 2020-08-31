ef4a8a0a34e56e954069767275de9c21192fb2a0
Olivia Cubela <oliviacubela@gmail.com>
2020-08-31 21:04:46 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,21 +10,21 @@ class Pokemon
     @db = db
   end
 
-  def self.save(name, type, db)
-    sql = <<-SQL
-      INSERT INTO pokemon (name, type) VALUES (?, ?);
-    SQL
-
-    db.execute(sql, [name, type])
-
-  end
+  # def self.save(name, type, db)
+  #   sql = <<-SQL
+  #     INSERT INTO pokemon (name, type) VALUES (?, ?);
+  #   SQL
+  #
+  #   db.execute(sql, [name, type])
+  #
+  # end
 
   def self.find(id, db)
     sql = <<-SQL
       SELECT * FROM pokemon WHERE id = (?);
     SQL
     pokemon = db.execute(sql, [id]).flatten
-    binding.pry
+
     Pokemon.new(name, type, db)
   end
 end
