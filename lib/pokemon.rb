2e7fb909872515b03f778cf4970aa6ff182a7cca
Olivia Cubela <oliviacubela@gmail.com>
2020-08-31 21:05:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,9 +14,9 @@ class Pokemon
   #   sql = <<-SQL
   #     INSERT INTO pokemon (name, type) VALUES (?, ?);
   #   SQL
-  #
+  # 
   #   db.execute(sql, [name, type])
-  #
+  # 
   # end
 
   def self.find(id, db)
